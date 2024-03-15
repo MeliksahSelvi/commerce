7ca39c3ce7b152d22d1252b6b320413202d301b0
mselvi <mselvi@iski.gov.tr>
2024-03-15 17:37:28 +0300

    added things to payment domain layer

---
+++ payment-service/payment-domain/src/main/java/com/commerce/payment/service/payment/usecase/AccountSave.java
@@ -0,0 +1,13 @@
+package com.commerce.payment.service.payment.usecase;
+
+import com.commerce.payment.service.common.model.UseCase;
+import com.commerce.payment.service.common.valueobject.CurrencyType;
+import com.commerce.payment.service.common.valueobject.Money;
+
+/**
+ * @Author mselvi
+ * @Created 12.03.2024
+ */
+
+public record AccountSave(Long customerId, Money currentBalance, CurrencyType currencyType) implements UseCase {
+}
