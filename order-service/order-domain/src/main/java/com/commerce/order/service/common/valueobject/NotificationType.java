1e4bb6ee1d372c4a5b29d6ed5c5ce00e302b30d0
mselvi <mselvi@iski.gov.tr>
2024-03-19 23:48:38 +0300

    added value objects

---
+++ order-service/order-domain/src/main/java/com/commerce/order/service/common/valueobject/NotificationType.java
@@ -0,0 +1,24 @@
+package com.commerce.order.service.common.valueobject;
+
+/**
+ * @Author mselvi
+ * @Created 08.03.2024
+ */
+
+public enum NotificationType {
+    APPROVING("APPROVING"),
+    SHIPPING("SHIPPING"),
+    DELIVERING("DELIVERING"),
+    CANCELLING("CANCELLING");
+
+    private String type;
+
+    NotificationType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
