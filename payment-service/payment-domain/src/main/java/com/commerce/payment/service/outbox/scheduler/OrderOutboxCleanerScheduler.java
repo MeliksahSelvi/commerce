ebcf20193349c13784f84edcb16a53c84f4b6408
mselvi <mselvi@iski.gov.tr>
2024-03-19 21:17:01 +0300

    added schedulers

---
+++ payment-service/payment-domain/src/main/java/com/commerce/payment/service/outbox/scheduler/OrderOutboxCleanerScheduler.java
@@ -0,0 +1,42 @@
+package com.commerce.payment.service.outbox.scheduler;
+
+import com.commerce.payment.service.common.DomainComponent;
+import com.commerce.payment.service.common.outbox.OutboxScheduler;
+import com.commerce.payment.service.common.outbox.OutboxStatus;
+import com.commerce.payment.service.outbox.entity.OrderOutbox;
+import com.commerce.payment.service.outbox.port.jpa.OrderOutboxDataPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Author mselvi
+ * @Created 07.03.2024
+ */
+
+@DomainComponent
+public class OrderOutboxCleanerScheduler implements OutboxScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderOutboxCleanerScheduler.class);
+    private final OrderOutboxDataPort dataPort;
+
+    public OrderOutboxCleanerScheduler(OrderOutboxDataPort dataPort) {
+        this.dataPort = dataPort;
+    }
+
+    @Override
+    @Scheduled(cron = "@midnight")
+    public void processOutboxMessage() {
+        Optional<List<OrderOutbox>> toBeProcessedOutboxMessages = dataPort.findByOutboxStatus(OutboxStatus.COMPLETED);
+
+        toBeProcessedOutboxMessages.ifPresent(orderOutboxMessages -> {
+            logger.info("Received {} OrderOutbox for clean-up", orderOutboxMessages.size());
+
+            dataPort.deleteByOutboxStatus(OutboxStatus.COMPLETED);
+            logger.info("{} OrderOutbox deleted!", orderOutboxMessages.size());
+        });
+    }
+}
---
+++ payment-service/payment-domain/src/main/java/com/commerce/payment/service/outbox/scheduler/OrderOutboxScheduler.java
@@ -0,0 +1,64 @@
+package com.commerce.payment.service.outbox.scheduler;
+
+import com.commerce.payment.service.common.DomainComponent;
+import com.commerce.payment.service.common.exception.PaymentDomainException;
+import com.commerce.payment.service.common.outbox.OutboxScheduler;
+import com.commerce.payment.service.common.outbox.OutboxStatus;
+import com.commerce.payment.service.outbox.entity.OrderOutbox;
+import com.commerce.payment.service.outbox.port.jpa.OrderOutboxDataPort;
+import com.commerce.payment.service.payment.port.messaging.output.PaymentResponseMessagePublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @Author mselvi
+ * @Created 07.03.2024
+ */
+
+@DomainComponent
+public class OrderOutboxScheduler implements OutboxScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderOutboxScheduler.class);
+    private final PaymentResponseMessagePublisher publisher;
+    private final OrderOutboxDataPort dataPort;
+
+    public OrderOutboxScheduler(PaymentResponseMessagePublisher publisher, OrderOutboxDataPort dataPort) {
+        this.publisher = publisher;
+        this.dataPort = dataPort;
+    }
+
+    @Override
+    @Transactional
+    @Scheduled(fixedDelayString = "${payment-service.outbox-scheduler-fixed-rate}",
+            initialDelayString = "${payment-service.outbox-scheduler-initial-delay}")
+    public void processOutboxMessage() {
+        Optional<List<OrderOutbox>> toBeProcessedOutboxMessages = dataPort.findByOutboxStatus(OutboxStatus.STARTED);
+
+        toBeProcessedOutboxMessages.ifPresent(orderOutboxMessages -> {
+            logger.info("Received {} OrderOutbox with ids: {}, sending to message bus!",
+                    orderOutboxMessages.size(),
+                    orderOutboxMessages.stream().map(outboxMessage -> outboxMessage.getId().toString())
+                            .collect(Collectors.joining(",")));
+
+            orderOutboxMessages.forEach(paymentOutboxMessage -> publisher.publish(paymentOutboxMessage, this::updateOutboxStatus));
+            logger.info("{} OrderOutbox sent to message bus!", orderOutboxMessages.size());
+        });
+    }
+
+    @Transactional
+    public void updateOutboxStatus(OrderOutbox orderOutbox, OutboxStatus outboxStatus) {
+        orderOutbox.setOutboxStatus(outboxStatus);
+        OrderOutbox savedOutbox = dataPort.save(orderOutbox);
+        if (savedOutbox == null) {
+            throw new PaymentDomainException(String.format("Could not save OrderOutbox with outbox id: %d",
+                    orderOutbox.getId()));
+        }
+        logger.info("OrderOutbox is updated with outbox status: {}", outboxStatus.name());
+    }
+}
