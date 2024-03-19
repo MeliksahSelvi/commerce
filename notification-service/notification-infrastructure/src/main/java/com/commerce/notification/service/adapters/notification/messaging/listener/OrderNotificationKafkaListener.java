5199131fc048316b69c8db07e85b618550815688
mselvi <mselvi@iski.gov.tr>
2024-03-19 23:00:23 +0300

    added kafka listeners

---
+++ notification-service/notification-infrastructure/src/main/java/com/commerce/notification/service/adapters/notification/messaging/listener/OrderNotificationKafkaListener.java
@@ -0,0 +1,76 @@
+package com.commerce.notification.service.adapters.notification.messaging.listener;
+
+import com.commerce.kafka.consumer.KafkaConsumer;
+import com.commerce.kafka.model.NotificationRequestAvroModel;
+import com.commerce.notification.service.common.valueobject.NotificationType;
+import com.commerce.notification.service.notification.port.messaging.input.OrderNotificationMessageListener;
+import com.commerce.notification.service.notification.usecase.OrderNotificationMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author mselvi
+ * @Created 08.03.2024
+ */
+
+@Component
+public class OrderNotificationKafkaListener implements KafkaConsumer<NotificationRequestAvroModel> {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderNotificationKafkaListener.class);
+    private final OrderNotificationMessageListener orderNotificationMessageListener;
+
+    public OrderNotificationKafkaListener(OrderNotificationMessageListener orderNotificationMessageListener) {
+        this.orderNotificationMessageListener = orderNotificationMessageListener;
+    }
+
+    @Override
+    @KafkaListener(id = "${kafka-consumer-config.order-notification-consumer-group-id}",
+            topics = "${notification-service.notification-request-topic-name}")
+    public void receive(@Payload List<NotificationRequestAvroModel> messages,
+                        @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
+                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+
+        logger.info("{} number of payment requests received with keys:{}, partitions:{} and offsets:{}",
+                messages.size(), keys, partitions, offsets);
+
+        for (NotificationRequestAvroModel avroModel : messages) {
+            OrderNotificationMessage message = buildUseCase(avroModel);
+            try {
+                switch (message.notificationType()) {
+                    case APPROVING -> {
+                        logger.info("Notification sending approving action");
+                        orderNotificationMessageListener.approving(message);
+                    }
+                    case SHIPPING -> {
+                        logger.info("Notification sending shipping action");
+                        orderNotificationMessageListener.shipping(message);
+                    }
+                    case DELIVERING -> {
+                        logger.info("Notification sending delivering action");
+                        orderNotificationMessageListener.delivering(message);
+                    }
+                    case CANCELLING -> {
+                        logger.info("Notification sending cancelling action");
+                        orderNotificationMessageListener.cancelling(message);
+                    }
+                }
+
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
+
+    private OrderNotificationMessage buildUseCase(NotificationRequestAvroModel avroModel) {
+        return new OrderNotificationMessage(avroModel.getOrderId(), avroModel.getCustomerId(),
+                NotificationType.valueOf(avroModel.getNotificationType().name()), avroModel.getMessage());
+    }
+}
