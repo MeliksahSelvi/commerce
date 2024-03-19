d52d404c1c8d46aeaf1d8bcebf5b8533548fa0f2
mselvi <mselvi@iski.gov.tr>
2024-03-19 23:55:32 +0300

    added schedulers

---
+++ order-service/order-domain/src/main/java/com/commerce/order/service/outbox/scheduler/InventoryOutboxCleanerScheduler.java
@@ -0,0 +1,67 @@
+package com.commerce.order.service.outbox.scheduler;
+
+import com.commerce.order.service.common.DomainComponent;
+import com.commerce.order.service.common.outbox.OutboxScheduler;
+import com.commerce.order.service.common.outbox.OutboxStatus;
+import com.commerce.order.service.common.saga.SagaStatus;
+import com.commerce.order.service.common.valueobject.OrderInventoryStatus;
+import com.commerce.order.service.outbox.entity.InventoryOutbox;
+import com.commerce.order.service.outbox.port.jpa.InventoryOutboxDataPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Author mselvi
+ * @Created 04.03.2024
+ */
+
+@DomainComponent
+public class InventoryOutboxCleanerScheduler implements OutboxScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(InventoryOutboxScheduler.class);
+    private final InventoryOutboxDataPort dataPort;
+
+    public InventoryOutboxCleanerScheduler(InventoryOutboxDataPort dataPort) {
+        this.dataPort = dataPort;
+    }
+
+    @Override
+    @Scheduled(cron = "@midnight")
+    public void processOutboxMessage() {
+        Optional<List<InventoryOutbox>> toBeProcessedOutboxMessages = dataPort.
+                findByOutboxStatusAndSagaStatuses(OutboxStatus.COMPLETED,
+                        SagaStatus.PAYING,
+                        SagaStatus.SUCCEEDED,
+                        SagaStatus.CANCELLED);
+
+        Optional<List<InventoryOutbox>> toBeTerminatedOutboxMessages = dataPort.
+                findByOutboxStatusAndSagaStatusAndOrderInventoryStatus(OutboxStatus.COMPLETED,
+                        SagaStatus.CANCELLING,
+                        OrderInventoryStatus.CHECKING_ROLLBACK);
+
+        toBeProcessedOutboxMessages.ifPresent(inventoryOutboxMessages -> {
+            logger.info("Received {} InventoryOutboxMessage for clean-up", inventoryOutboxMessages.size());
+
+            dataPort.deleteByOutboxStatusAndSagaStatuses(
+                    OutboxStatus.COMPLETED,
+                    SagaStatus.PAYING,
+                    SagaStatus.SUCCEEDED,
+                    SagaStatus.CANCELLED);
+            logger.info("{} InventoryOutboxMessage deleted!", inventoryOutboxMessages.size());
+        });
+
+        toBeTerminatedOutboxMessages.ifPresent(inventoryOutboxMessages -> {
+            logger.info("Received {} InventoryOutboxMessage for clean-up", inventoryOutboxMessages.size());
+
+            dataPort.deleteByOutboxStatusAndSagaStatusAndOrderInventoryStatus(
+                    OutboxStatus.COMPLETED,
+                    SagaStatus.CANCELLING,
+                    OrderInventoryStatus.CHECKING_ROLLBACK);
+            logger.info("{} InventoryOutboxMessage deleted!", inventoryOutboxMessages.size());
+        });
+    }
+}
---
+++ order-service/order-domain/src/main/java/com/commerce/order/service/outbox/scheduler/InventoryOutboxScheduler.java
@@ -0,0 +1,64 @@
+package com.commerce.order.service.outbox.scheduler;
+
+import com.commerce.order.service.common.DomainComponent;
+import com.commerce.order.service.common.outbox.OutboxScheduler;
+import com.commerce.order.service.common.outbox.OutboxStatus;
+import com.commerce.order.service.common.saga.SagaStatus;
+import com.commerce.order.service.outbox.entity.InventoryOutbox;
+import com.commerce.order.service.outbox.port.jpa.InventoryOutboxDataPort;
+import com.commerce.order.service.order.port.messaging.output.InventoryRequestMessagePublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @Author mselvi
+ * @Created 04.03.2024
+ */
+
+@DomainComponent
+public class InventoryOutboxScheduler implements OutboxScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(InventoryOutboxScheduler.class);
+    private final InventoryOutboxDataPort dataPort;
+    private final InventoryRequestMessagePublisher publisher;
+
+    public InventoryOutboxScheduler(InventoryOutboxDataPort dataPort, InventoryRequestMessagePublisher publisher) {
+        this.dataPort = dataPort;
+        this.publisher = publisher;
+    }
+
+    @Override
+    @Transactional
+    @Scheduled(fixedDelayString = "${order-service.outbox-scheduler-fixed-rate}",
+            initialDelayString = "${order-service.outbox-scheduler-initial-delay}")
+    public void processOutboxMessage() {
+        Optional<List<InventoryOutbox>> toBeProcessedOutboxMessages = dataPort
+                .findByOutboxStatusAndSagaStatuses(OutboxStatus.STARTED,
+                        SagaStatus.CHECKING,
+                        SagaStatus.PROCESSING,
+                        SagaStatus.CANCELLING);
+
+        toBeProcessedOutboxMessages.ifPresent(inventoryOutboxMessages -> {
+            logger.info("Received {} InventoryOutbox with ids: {}, sending to message bus!",
+                    inventoryOutboxMessages.size(),
+                    inventoryOutboxMessages.stream().map(outboxMessage -> outboxMessage.getId().toString())
+                            .collect(Collectors.joining(",")));
+
+            inventoryOutboxMessages.forEach(inventoryOutboxMessage -> publisher.publish(inventoryOutboxMessage, this::updateOutboxStatus));
+            logger.info("{} InventoryOutbox sent to message bus!", inventoryOutboxMessages.size());
+        });
+    }
+
+    @Transactional
+    public void updateOutboxStatus(InventoryOutbox inventoryOutbox, OutboxStatus outboxStatus) {
+        inventoryOutbox.setOutboxStatus(outboxStatus);
+        dataPort.save(inventoryOutbox);
+        logger.info("InventoryOutboxMessage is updated with outbox status: {}", outboxStatus.name());
+    }
+}
