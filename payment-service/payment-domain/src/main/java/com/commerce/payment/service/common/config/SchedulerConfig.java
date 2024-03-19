282b485b169300d879cf4172d846489e50193a22
mselvi <mselvi@iski.gov.tr>
2024-03-19 21:18:12 +0300

    added scheduler config

---
+++ payment-service/payment-domain/src/main/java/com/commerce/payment/service/common/config/SchedulerConfig.java
@@ -0,0 +1,14 @@
+package com.commerce.payment.service.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Author mselvi
+ * @Created 07.03.2024
+ */
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
