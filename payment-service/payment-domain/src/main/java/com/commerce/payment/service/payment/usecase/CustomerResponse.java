919bd270855230d1089e507b557ac2b79b6be837
mselvi <mselvi@iski.gov.tr>
2024-03-21 16:16:21 +0300

    changed inner rest calls response

---
+++ payment-service/payment-domain/src/main/java/com/commerce/payment/service/payment/usecase/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.commerce.payment.service.payment.usecase;
+
+import com.commerce.payment.service.common.model.UseCase;
+
+/**
+ * @Author mselvi
+ * @Created 21.03.2024
+ */
+
+public record CustomerResponse(Long id, String firstName, String lastName, String identityNo, String email) implements UseCase {
+}
