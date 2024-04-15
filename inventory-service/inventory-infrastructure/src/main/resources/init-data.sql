85aaa17ebc917ed06872b92691100d7b164e6cfc
mselvi <mselvi@iski.gov.tr>
2024-04-15 17:45:14 +0300

    removed dependency

--- inventory-service/inventory-infrastructure/src/main/resources/init-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO inventory.product (id,name,price,quantity,availability,status_type,created_at,updated_at)
-values
-(1,'product-1',25.0,100,true,'ACTIVE',current_timestamp,null),
-(2,'product-2',50.0,100,true,'ACTIVE',current_timestamp,null);
--- payment-service/payment-infrastructure/src/main/resources/init-data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO payment.account(id, customer_id, current_balance,currency_type,iban_no,cancel_date,status_type,created_at,updated_at)
-VALUES (1,1,10000.00,'TL','TR260006278794193236123947',null,'ACTIVE',current_timestamp,null);
-
-INSERT INTO payment.account_activity(id, account_id, cost,transaction_date,current_balance,activity_type,status_type,created_at,updated_at)
-VALUES (1,1,10000.00,current_timestamp,10000.00,'DEPOSIT','ACTIVE',current_timestamp,null);
-
-
-
-
